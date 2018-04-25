dfe8438fb2eed43ef93db795772c30e6d206c1e4
uandhgroup <36882850+uandhgroup@users.noreply.github.com>
2018-04-25 17:54:13 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -58,7 +58,7 @@ function onOffline() {
 }
 
 function openBrowser() {
-   var url = 'https://dusthqdenevents.frb.io/';
+   var url = 'https://uandh.in/';
    var target = '_blank';
    var options = "location=no,toolbar=no"
    var ref = cordova.InAppBrowser.open(url, target, options);
